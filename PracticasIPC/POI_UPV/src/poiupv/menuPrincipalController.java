768668c8df7dbe812a42926828dfaa59fd89242e
providiego <diegomln.28@gmail.com>
2025-05-31 18:21:38 +0200

    menu principal

---
+++ PracticasIPC/POI_UPV/src/poiupv/menuPrincipalController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poiupv;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author Diego Molina
+ */
+public class menuPrincipalController {
+
+    @FXML
+    private ImageView avatarImageView;
+    @FXML
+    private Label bienvenidaLabel;
+    @FXML
+    private Button ejerciciosBtn;
+    @FXML
+    private Button estadisticasBtn;
+    @FXML
+    private Button modificarPerfilBtn;
+    @FXML
+    private Button cerrarSesionBtn;
+    
+}
